80e5954ae5a0342479a11adf559cdd32f676d195
charleschou56 <48513044+charleschou56@users.noreply.github.com>
2020-03-11 16:12:00 +0800

    Add device info to test report (#21)

--- gen_report.py
+++ gen_report.py
@@ -8,13 +8,23 @@ import requests
 import base64
 from datetime import datetime
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, exists
+from distutils.dir_util import copy_tree, remove_tree
 
 config = {}
 config ['controller_host'] = '127.0.0.1'
 
-xml_file_path = './xunit/'
+# datetime object containing current date and time
+now = datetime.now()
+
+src_xml_dir = './xunit/'
+dst_xml_dir = './xunit-{}/'.format(now.strftime("%Y-%m-%d"))
 URL = "http://" + config['controller_host'] + ":8181/mars/"
+
+# copy source files into new folder
+if exists(dst_xml_dir):
+    remove_tree(dst_xml_dir)
+copy_tree(src_xml_dir, dst_xml_dir)
 
 # admin username
 ADMIN_USERNAME = 'karaf'
@@ -25,14 +35,14 @@ LOGIN = base64.b64encode(bytes('{}:{}'.format(ADMIN_USERNAME, ADMIN_PASSWORD)))
 AUTH_TOKEN = 'BASIC ' + LOGIN
 GET_HEADER = {'Authorization': AUTH_TOKEN}
 
-# datetime object containing current date and time
-now = datetime.now()
-
 # log file name
 log_file_name = now.strftime("%Y%m%d-%H%M%S")
 
 # mars version
-response = requests.get(URL+"utility/v1/version", headers=GET_HEADER)
+version = requests.get(URL+"utility/v1/version", headers=GET_HEADER)
+
+# devices info
+devices_info = requests.get(URL+"v1/devices", headers=GET_HEADER)
 
 fp = open('test-report-{}.log'.format(log_file_name), "a")
 
@@ -42,16 +52,24 @@ dt_string = now.strftime("%Y/%m/%d %H:%M:%S")
 print >>fp, 'Report generated at {}'.format(dt_string)
 print >>fp, ''
 print >>fp, '<<< Mars Informantion >>>'
-print >>fp, 'Git Commit           : {}'.format(response.json()['commit'])
-print >>fp, 'Version              : {}'.format(response.json()['version'])
-print >>fp, 'Build server         : {}'.format(response.json()['build_server'])
-print >>fp, 'Build time           : {}'.format(response.json()['build_date'])
-print >>fp, 'Logstash version     : {}'.format(response.json()['logstash'])
-print >>fp, 'Elasticsearch version: {}'.format(response.json()['elasticsearch'])
-print >>fp, 'Nginx version        : {}'.format(response.json()['nginx'])
+print >>fp, 'Git Commit           : {}'.format(version.json()['commit'])
+print >>fp, 'Version              : {}'.format(version.json()['version'])
+print >>fp, 'Build server         : {}'.format(version.json()['build_server'])
+print >>fp, 'Build time           : {}'.format(version.json()['build_date'])
+print >>fp, 'Logstash version     : {}'.format(version.json()['logstash'])
+print >>fp, 'Elasticsearch version: {}'.format(version.json()['elasticsearch'])
+print >>fp, 'Nginx version        : {}'.format(version.json()['nginx'])
 print >>fp, ''
 
-xml_files = [f for f in listdir(xml_file_path) if isfile(join(xml_file_path, f))]
+print >>fp, '<<< Devices Informantion >>>'
+for i in range(4):
+    print >>fp, 'ID: {}'.format(devices_info.json()['devices'][i]['id']),
+    print >>fp, '  Serial: {}'.format(devices_info.json()['devices'][i]['serial']),
+    print >>fp, '  NOS: {}'.format(devices_info.json()['devices'][i]['nos']),
+    print >>fp, '  Software: {}'.format(devices_info.json()['devices'][i]['sw'])
+print >>fp, ''
+
+xml_files = [f for f in listdir(dst_xml_dir) if isfile(join(dst_xml_dir, f))]
 xml_files.sort()
 
 # print xml_files
@@ -60,7 +78,7 @@ print >>fp, '<<< Test Result >>>'
 result = {'pass': 0, 'error': 0, 'failure': 0, 'check': 0}
 
 for xml_file in xml_files:
-    ts, tr = xunitparser.parse(open(xml_file_path + xml_file))
+    ts, tr = xunitparser.parse(open(dst_xml_dir + xml_file))
 
     if len(tr.errors) == 0 and len(tr.failures) == 0:
         test_result = 'PASS'
